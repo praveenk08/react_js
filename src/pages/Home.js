a86f63959b506cbb94a3f0a50144bb0e6705d22b
Praveen Kumar <pk46066@gmail.com>
2024-05-07 14:15:33 +0530

    link tag home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,6 +2,7 @@ import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 
+import { Link } from "react-router-dom";
 class Home extends React.Component {
   render() {
     return (
